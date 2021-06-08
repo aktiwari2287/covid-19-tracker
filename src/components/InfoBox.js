89426a081b3de51724801035ede9c19dd454d4d8
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-08 12:41:58 +0530

    Added some styles

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
-export default function InfoBox({ title, cases ,isRed,active, total, ...props }) {
+export default function InfoBox({ title, cases, isRed, active, total, ...props }) {
+  console.log("active ", active);
   return (
-    <Card className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}>
+    <Card className={`infoBox ${active ? isRed ? 'infoBox--red':'infoBox--selected':''}`}>
       <CardContent onClick={props.onClick}>
         <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
         <h2 className="infoBox__cases"> {cases}</h2>
